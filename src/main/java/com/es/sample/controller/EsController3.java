90fd1302c0c9c94fafd0504cd75381f770ba4e66
ShipJH <ShipJH@CARROT101-PC>
2019-06-27 17:36:14 +0900

    tl

--- src/main/java/com/es/sample/controller/EsController3.java
+++ src/main/java/com/es/sample/controller/EsController3.java
@@ -1,20 +1,16 @@
 package com.es.sample.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.es.sample.request.PersonSaveReq;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EsController3 {
 
-	@GetMapping("/testttt")
-	public ResponseEntity<?> insertPerson(@RequestBody PersonSaveReq personSaveReq){
-
-		return new ResponseEntity<>("zzz", HttpStatus.OK);
-	}
+	@RequestMapping(value = "/zzz", method = RequestMethod.GET)
+    public String test() {
+    		
+    	return "views/es-sample";
+    }
 	
 }
